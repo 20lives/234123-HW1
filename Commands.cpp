0747cc613e2550bd6b1bf17d2c0a401aacd2dcc7
Naomi <naomiriv@campus.technion.ac.il>
2020-05-04 12:47:37 +0000

    cleaning up

--- Commands.cpp
+++ Commands.cpp
@@ -14,10 +14,10 @@ const std::string WHITESPACE = " \n\r\t\f\v";
 
 #if 0
 #define FUNC_ENTRY()  \
-  cerr << __PRETTY_FUNCTION__ << " --> " << endl;
+    cerr << __PRETTY_FUNCTION__ << " --> " << endl;
 
 #define FUNC_EXIT()  \
-  cerr << __PRETTY_FUNCTION__ << " <-- " << endl;
+    cerr << __PRETTY_FUNCTION__ << " <-- " << endl;
 #else
 #define FUNC_ENTRY()
 #define FUNC_EXIT()
@@ -26,12 +26,16 @@ const std::string WHITESPACE = " \n\r\t\f\v";
 #define DEBUG_PRINT cerr << "DEBUG: "
 
 #define EXEC(path, arg) \
-  execvp((path), (arg));
+    execvp((path), (arg));
+
+// should be in cdCommand class maybe?
+enum CD_TYPE {
+    ePrevDir, eChangeDir
+};
 
 // converts character array
 // to string and returns it
-string _convertToString(char* a, int size)
-{
+string _convertToString(char* a, int size) {
     int i;
     string s = "";
     for (i = 0; i < size; i++) {
@@ -40,62 +44,56 @@ string _convertToString(char* a, int size)
     return s;
 }
 
-string _ltrim(const std::string& s)
-{
-  size_t start = s.find_first_not_of(WHITESPACE);
-  return (start == std::string::npos) ? "" : s.substr(start);
+string _ltrim(const std::string& s) {
+    size_t start = s.find_first_not_of(WHITESPACE);
+    return (start == std::string::npos) ? "" : s.substr(start);
 }
 
-string _rtrim(const std::string& s)
-{
-  size_t end = s.find_last_not_of(WHITESPACE);
-  return (end == std::string::npos) ? "" : s.substr(0, end + 1);
+string _rtrim(const std::string& s) {
+    size_t end = s.find_last_not_of(WHITESPACE);
+    return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
 
-string _trim(const std::string& s)
-{
-  return _rtrim(_ltrim(s));
+string _trim(const std::string& s) {
+    return _rtrim(_ltrim(s));
 }
 
 int _parseCommandLine(const char* cmd_line, char** args) {
-  FUNC_ENTRY()
-  int i = 0;
-  std::istringstream iss(_trim(string(cmd_line)).c_str());
-  for(std::string s; iss >> s; ) {
-    args[i] = (char*)malloc(s.length()+1);
-    memset(args[i], 0, s.length()+1);
-    strcpy(args[i], s.c_str());
-    args[++i] = NULL;
-  }
-  return i;
-
-  FUNC_EXIT()
+    FUNC_ENTRY()
+    int i = 0;
+    std::istringstream iss(_trim(string(cmd_line)).c_str());
+    for(std::string s; iss >> s; ) {
+        args[i] = (char*)malloc(s.length()+1);
+        memset(args[i], 0, s.length()+1);
+        strcpy(args[i], s.c_str());
+        args[++i] = NULL;
+    }
+    return i;
+    FUNC_EXIT()
 }
 
 bool _isBackgroundComamnd(const char* cmd_line) {
-  const string str(cmd_line);
-  return str[str.find_last_not_of(WHITESPACE)] == '&';
+    const string str(cmd_line);
+    return str[str.find_last_not_of(WHITESPACE)] == '&';
 }
 
 void _removeBackgroundSign(char* cmd_line) {
-  const string str(cmd_line);
-  // find last character other than spaces
-  unsigned int idx = str.find_last_not_of(WHITESPACE);
-  // if all characters are spaces then return
-  if (idx == string::npos) {
-    return;
-  }
-  // if the command line does not end with & then return
-  if (cmd_line[idx] != '&') {
-    return;
-  }
-  // replace the & (background sign) with space and then remove all tailing spaces.
-  cmd_line[idx] = ' ';
-  // truncate the command line string up to the last non-space character
-  cmd_line[str.find_last_not_of(WHITESPACE, idx) + 1] = 0;
+    const string str(cmd_line);
+    // find last character other than spaces
+    unsigned int idx = str.find_last_not_of(WHITESPACE);
+    // if all characters are spaces then return
+    if (idx == string::npos) {
+        return;
+    }
+    // if the command line does not end with & then return
+    if (cmd_line[idx] != '&') {
+        return;
+    }
+    // replace the & (background sign) with space and then remove all tailing spaces.
+    cmd_line[idx] = ' ';
+    // truncate the command line string up to the last non-space character
+    cmd_line[str.find_last_not_of(WHITESPACE, idx) + 1] = 0;
 }
-
-// TODO: Add your implementation for classes in Commands.h 
 
 SmallShell::SmallShell() {
 // TODO: add your implementation
@@ -112,18 +110,18 @@ string SmallShell::getPrompt() {
 /**
 * Creates and returns a pointer to Command class which matches the given command line (cmd_line)
 */
-Command * SmallShell::CreateCommand(const char* cmd_line) {
-  string cmd_s = string(cmd_line);
-  if (cmd_s.find("chprompt") == 0) {
-      return new ChangePromptCommand(cmd_line);
-  } else if (cmd_s.find("showpid") == 0) {
-      return new ShowPidCommand(cmd_line);
-  } else if (cmd_s.find("pwd") == 0) {
-      return new PwdCommand(cmd_line);
-  } else if (cmd_s.find("cd") == 0) {
-      return new CdCommand(cmd_line);
-  }
-  return nullptr;
+Command *SmallShell::CreateCommand(const char* cmd_line) {
+    string cmd_s = string(cmd_line);
+    if (cmd_s.find("chprompt") == 0) {
+        return new ChangePromptCommand(cmd_line);
+    } else if (cmd_s.find("showpid") == 0) {
+        return new ShowPidCommand(cmd_line);
+    } else if (cmd_s.find("pwd") == 0) {
+        return new PwdCommand(cmd_line);
+    } else if (cmd_s.find("cd") == 0) {
+        return new CdCommand(cmd_line);
+    }
+    return nullptr;
 }
 
 Command::Command(const char* cmd_line) {
@@ -149,7 +147,6 @@ BuiltInCommand::~BuiltInCommand() {
 ChangePromptCommand::ChangePromptCommand(const char* cmd_line) : BuiltInCommand(cmd_line) {
 
 }
-
 
 ShowPidCommand::ShowPidCommand(const char* cmd_line) : BuiltInCommand(cmd_line) {
 
@@ -182,10 +179,6 @@ void PwdCommand::execute() {
     std::cout << currWorkingDir << "\n";
 }
 
-enum CD_TYPE {
-    ePrevDir, eParentDir
-};
-
 void  _changeDirectory(CD_TYPE cdType, char* dir = nullptr) {
     SmallShell& smash = SmallShell::getInstance();
     char currWorkingDir[PATH_MAX] = {};
@@ -211,20 +204,23 @@ void CdCommand::execute() {
         if (strcmp(getArg(1), "-") == 0) {
             _changeDirectory(ePrevDir);
         } else {
-            _changeDirectory(eParentDir, getArg(1));
+            _changeDirectory(eChangeDir, getArg(1));
         }
     }
 }
 
+/**
+ * SmallShell class implementations
+ */
 void SmallShell::executeCommand(const char *cmd_line) {
-  // TODO: Add your implementation here
+    // TODO: Add your implementation here
 
-  // for example:
-  Command* cmd = CreateCommand(cmd_line);
-  if (cmd) {
-    cmd->execute();
-  }
-  // Please note that you must fork smash process for some commands (e.g., external commands....)
+    // for example:
+    Command* cmd = CreateCommand(cmd_line);
+    if (cmd) {
+        cmd->execute();
+    }
+// Please note that you must fork smash process for some commands (e.g., external commands....)
 };
 
 void SmallShell::setPrompt(const char *_prompt) {
--- Commands.h
+++ Commands.h
@@ -9,76 +9,62 @@
 #define HISTORY_MAX_RECORDS (50)
 
 class Command {
-// TODO: Add your data members
+private:
     char *argc[COMMAND_MAX_ARGS] = {};
     int argv;
- public:
-  Command(const char* cmd_line);
-  virtual ~Command() {};
-  virtual void execute() = 0;
-  //virtual void prepare();
-  //virtual void cleanup();
-  char* getArg(int i);
-  int getArgCount();
+public:
+    Command(const char* cmd_line);
+    virtual ~Command() {};
+    virtual void execute() = 0;
+    //virtual void prepare();
+    //virtual void cleanup();
+    char* getArg(int i);
+    int getArgCount();
 };
 
 class BuiltInCommand : public Command {
- public:
-  BuiltInCommand(const char* cmd_line);
-  virtual ~BuiltInCommand();
+public:
+    BuiltInCommand(const char* cmd_line);
+    virtual ~BuiltInCommand();
 };
 
 class ExternalCommand : public Command {
- public:
-  ExternalCommand(const char* cmd_line);
-  virtual ~ExternalCommand() {}
-  void execute() override;
+public:
+    ExternalCommand(const char* cmd_line);
+    virtual ~ExternalCommand() {}
+    void execute() override;
 };
 
 class PipeCommand : public Command {
-  // TODO: Add your data members
- public:
-  PipeCommand(const char* cmd_line);
-  virtual ~PipeCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    PipeCommand(const char* cmd_line);
+    virtual ~PipeCommand() {}
+    void execute() override;
 };
 
 class RedirectionCommand : public Command {
- // TODO: Add your data members
- public:
-  explicit RedirectionCommand(const char* cmd_line);
-  virtual ~RedirectionCommand() {}
-  void execute() override;
-  //void prepare() override;
-  //void cleanup() override;
-};
-
-class ChangeDirCommand : public BuiltInCommand {
-// TODO: Add your data members public:
-  ChangeDirCommand(const char* cmd_line, char** plastPwd);
-  virtual ~ChangeDirCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    explicit RedirectionCommand(const char* cmd_line);
+    virtual ~RedirectionCommand() {}
+    void execute() override;
+    //void prepare() override;
+    //void cleanup() override;
 };
 
 class ChangePromptCommand : public BuiltInCommand {
-public:
+    public:
     ChangePromptCommand(const char* cmd_line);
     virtual ~ChangePromptCommand() {}
     void execute() override;
 };
 
-class GetCurrDirCommand : public BuiltInCommand {
- public:
-  GetCurrDirCommand(const char* cmd_line);
-  virtual ~GetCurrDirCommand() {}
-  void execute() override;
-};
-
 class ShowPidCommand : public BuiltInCommand {
- public:
-  ShowPidCommand(const char* cmd_line);
-  virtual ~ShowPidCommand() {}
-  void execute() override;
+public:
+    ShowPidCommand(const char* cmd_line);
+    virtual ~ShowPidCommand() {}
+    void execute() override;
 };
 
 class PwdCommand : public BuiltInCommand {
@@ -97,120 +83,119 @@ public:
 
 class JobsList;
 class QuitCommand : public BuiltInCommand {
-// TODO: Add your data members public:
-  QuitCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~QuitCommand() {}
-  void execute() override;
+    // TODO: Add your data members public:
+    QuitCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~QuitCommand() {}
+    void execute() override;
 };
 
 class CommandsHistory {
- protected:
-  class CommandHistoryEntry {
-	  // TODO: Add your data members
-  };
- // TODO: Add your data members
- public:
-  CommandsHistory();
-  ~CommandsHistory() {}
-  void addRecord(const char* cmd_line);
-  void printHistory();
+protected:
+    class CommandHistoryEntry {
+        // TODO: Add your data members
+    };
+// TODO: Add your data members
+public:
+    CommandsHistory();
+    ~CommandsHistory() {}
+    void addRecord(const char* cmd_line);
+    void printHistory();
 };
 
 class HistoryCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  HistoryCommand(const char* cmd_line, CommandsHistory* history);
-  virtual ~HistoryCommand() {}
-  void execute() override;
+// TODO: Add your data members
+public:
+    HistoryCommand(const char* cmd_line, CommandsHistory* history);
+    virtual ~HistoryCommand() {}
+    void execute() override;
 };
 
 class JobsList {
- public:
-  class JobEntry {
-   // TODO: Add your data members
-  };
- // TODO: Add your data members
- public:
-  JobsList();
-  ~JobsList();
-  void addJob(Command* cmd, bool isStopped = false);
-  void printJobsList();
-  void killAllJobs();
-  void removeFinishedJobs();
-  JobEntry * getJobById(int jobId);
-  void removeJobById(int jobId);
-  JobEntry * getLastJob(int* lastJobId);
-  JobEntry *getLastStoppedJob(int *jobId);
-  // TODO: Add extra methods or modify exisitng ones as needed
+public:
+    class JobEntry {
+        // TODO: Add your data members
+    };
+    // TODO: Add your data members
+public:
+    JobsList();
+    ~JobsList();
+    void addJob(Command* cmd, bool isStopped = false);
+    void printJobsList();
+    void killAllJobs();
+    void removeFinishedJobs();
+    JobEntry * getJobById(int jobId);
+    void removeJobById(int jobId);
+    JobEntry * getLastJob(int* lastJobId);
+    JobEntry *getLastStoppedJob(int *jobId);
+    // TODO: Add extra methods or modify exisitng ones as needed
 };
 
 class JobsCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  JobsCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~JobsCommand() {}
-  void execute() override;
+// TODO: Add your data members
+public:
+    JobsCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~JobsCommand() {}
+    void execute() override;
 };
 
 class KillCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  KillCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~KillCommand() {}
-  void execute() override;
+    // TODO: Add your data members
+public:
+    KillCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~KillCommand() {}
+    void execute() override;
 };
 
 class ForegroundCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  ForegroundCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~ForegroundCommand() {}
-  void execute() override;
+// TODO: Add your data members
+public:
+    ForegroundCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~ForegroundCommand() {}
+    void execute() override;
 };
 
 class BackgroundCommand : public BuiltInCommand {
- // TODO: Add your data members
- public:
-  BackgroundCommand(const char* cmd_line, JobsList* jobs);
-  virtual ~BackgroundCommand() {}
-  void execute() override;
+// TODO: Add your data members
+public:
+    BackgroundCommand(const char* cmd_line, JobsList* jobs);
+    virtual ~BackgroundCommand() {}
+    void execute() override;
 };
 
 
 // TODO: should it really inhirit from BuiltInCommand ?
 class CopyCommand : public BuiltInCommand {
- public:
-  CopyCommand(const char* cmd_line);
-  virtual ~CopyCommand() {}
-  void execute() override;
+public:
+    CopyCommand(const char* cmd_line);
+    virtual ~CopyCommand() {}
+    void execute() override;
 };
 
 // TODO: add more classes if needed 
 // maybe chprompt , timeout ?
 
 class SmallShell {
- private:
+private:
     std::string prevDir = "";
-  std::string prompt = "smash";
-  SmallShell();
- public:
+    std::string prompt = "smash";
+    SmallShell();
+public:
     bool isPrevDirSet = false;
     Command *CreateCommand(const char* cmd_line);
-  SmallShell(SmallShell const&)      = delete; // disable copy ctor
-  void operator=(SmallShell const&)  = delete; // disable = operator
-  static SmallShell& getInstance() // make SmallShell singleton
-  {
+    SmallShell(SmallShell const&)      = delete; // disable copy ctor
+    void operator=(SmallShell const&)  = delete; // disable = operator
+    static SmallShell& getInstance() {
     static SmallShell instance; // Guaranteed to be destroyed.
     // Instantiated on first use.
     return instance;
-  }
-  std::string getPrompt();
-  void setPrompt(const char* _prompt);
-  std::string getPrevDir();
-  void setPrevDir(const char* _dir);
-  ~SmallShell();
-  void executeCommand(const char* cmd_line);
-  // TODO: add extra methods as needed
+    }
+    void executeCommand(const char* cmd_line);
+    std::string getPrompt();
+    void setPrompt(const char* _prompt);
+    std::string getPrevDir();
+    void setPrevDir(const char* _dir);
+    ~SmallShell();
+    // TODO: add extra methods as needed
 };
 
 #endif //SMASH_COMMAND_H_
