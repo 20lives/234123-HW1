b6544fb021761d2a1dd25fd71dafd048f4a657e5
Naomi <naomiriv@campus.technion.ac.il>
2020-05-08 11:43:20 +0000

    adding fgJob class and handle ctrl-c

---
+++ FgJob.cpp
@@ -0,0 +1,35 @@
+//
+// Created by nomi on 5/8/20.
+//
+#include <string.h>
+
+#include "FgJob.h"
+
+FgJob::FgJob() {
+
+}
+
+FgJob::~FgJob() {
+
+}
+
+void FgJob::updateFg(const char *_cmdLine, pid_t _pid) {
+    isFgJob = true;
+    strcpy(cmdLine, _cmdLine);
+    pid = _pid;
+}
+
+pid_t FgJob::getPid() {
+    return pid;
+}
+
+void FgJob::clearFg() {
+    isFgJob = false;
+    memset(cmdLine, 0, 255);
+    cmdLineLength = 0;
+    pid = -1;
+}
+
+bool FgJob::isFgJobRunning() {
+    return isFgJob;
+}
---
+++ FgJob.h
@@ -0,0 +1,35 @@
+//
+// Created by nomi on 5/8/20.
+//
+
+#ifndef HW1_234123_FGJOB_H
+#define HW1_234123_FGJOB_H
+
+#include "Utilities.h"
+
+class FgJob {
+private:
+    bool isFgJob = false;
+
+    char cmdLine[COMMAND_ARGS_MAX_LENGTH] = {};
+    int cmdLineLength = 0;
+
+    pid_t pid = -1;
+
+    FgJob();
+public:
+    FgJob(FgJob const&) = delete;
+    void operator=(FgJob const&) = delete;
+    static FgJob& getInstance() {
+        static FgJob instance;
+        return instance;
+    }
+    void updateFg(const char* _cmdLine, pid_t _pid);
+    void clearFg();
+    pid_t getPid();
+    bool isFgJobRunning();
+    ~FgJob();
+};
+
+
+#endif //HW1_234123_FGJOB_H
