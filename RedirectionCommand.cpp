4ea261fb4798d390c863ec9f0e95681ee5e6ec06
Naomi <naomiriv@campus.technion.ac.il>
2020-05-10 07:07:03 +0000

    adding parse to redirection

--- RedirectionCommand.cpp
+++ RedirectionCommand.cpp
@@ -3,6 +3,9 @@
 //
 #include <string.h>
 
+#include <string.h>
+#include <iostream>
+
 #include "RedirectionCommand.h"
 #include "Utilities.h"
 
@@ -10,6 +13,21 @@ RedirectionCommand::RedirectionCommand(const char *cmd_line, bool _isBackground,
     strcpy(rawCmdLine, cmd_line);
     isBackground = _isBackground;
     isAppend = _isAppend;
+
+    char cmdLine[COMMAND_ARGS_MAX_LENGTH] = {};
+    strcpy(cmdLine, cmd_line);
+    _removeBackgroundSign(cmdLine);
+
+    std::size_t found = string(cmd_line).find_first_of('>');
+    if (!_isAppend) {
+        strcpy(cmd, string(cmdLine).substr(0, found).c_str());
+        strcpy(file, string(cmdLine).substr(found + 1).c_str());
+    } else {
+            strcpy(cmd, string(cmdLine).substr(0, found).c_str());
+            strcpy(file, string(cmdLine).substr(found + 2).c_str());
+    }
+    std::cout << "cmd: " << cmd << "file: " << file << '\n';
+
 }
 
 void RedirectionCommand::execute() {
